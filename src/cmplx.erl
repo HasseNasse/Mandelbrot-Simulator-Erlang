ec90ae730a09c478da09977846237447a80303bd
Hassan Nazar <hassan.nazar94@gmail.com>
2015-02-16 17:20:55 +0100

    initial push

---
+++ src/cmplx.erl
@@ -0,0 +1,33 @@
+%% @author HassanNazar
+%% @doc @todo Add description to cmplx.
+
+
+-module(cmplx).
+-compile(export_all).
+
+%% ====================================================================
+%% API functions
+%% ====================================================================
+
+%returns the complex number with real value X and imaginary Y
+new(X, Y)->
+	na.
+
+% Addition of two complex numbers A,B
+add(A, B)->
+	na.
+
+% Will return the sqr-root of A
+sqr(A)->
+	A*A.
+
+% Will return the absolute Val. of A
+abs(A)->
+	%Use sqrt/1 func from Math lib when counting abs value........
+	na.
+
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+
