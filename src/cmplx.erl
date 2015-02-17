ae2ae3c0fad8e60a70efc6195f58072679850ae5
Hassan Nazar <hassan.nazar94@gmail.com>
2015-02-17 09:17:06 +0100

    09:16

--- src/cmplx.erl
+++ src/cmplx.erl
@@ -1,5 +1,4 @@
 %% @author HassanNazar
-%% @doc @todo Add description to cmplx.
 
 
 -module(cmplx).
@@ -11,20 +10,41 @@
 
 %returns the complex number with real value X and imaginary Y
 new(X, Y)->
-	na.
+	{X,Y}.
 
 % Addition of two complex numbers A,B
-add(A, B)->
-	na.
+add(A,B)->
+	case A of
+		{A1,A2}->
+			case B of
+				{B1,B2}->
+					{A1+B1,A2+B2}; % (A+Bi)+(C+Di)=(A+C)+(B+D)i
+			true->
+				'Not a valid input!'
+			end;
+		true-> 
+			'Not a valid input!'
+	end.
 
-% Will return the sqr-root of A
+% Will return the sqr of A
 sqr(A)->
-	A*A.
+	case A of
+		{A1,A2}->
+			{((A1*A1)+(A2*A2))*(-1)}; % (A+Bi)²=-1(A²+B²)
+		true-> 
+			'Not a valid input!'
+	end.
 
 % Will return the absolute Val. of A
 abs(A)->
+	case A of
+		{A1,A2}->
+			math:sqrt(((A1*A1)+(A2*A2)));
+		true-> 
+			'Not a valid input!'
+	end.
 	%Use sqrt/1 func from Math lib when counting abs value........
-	na.
+	
 
 %% ====================================================================
 %% Internal functions
